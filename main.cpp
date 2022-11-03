a4c6ba6f53a3aea646ad264534668e8f5d39bcde
sdi1900034 <sdi1900034@di.uoa.gr>
2022-11-03 17:37:29 +0200

    add convex hull

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 int main() 
 {
 
+    int j=0;
+    for(i
+        for(auto it = CV.begin(); it!= CV.end();++it){
+            if(i==0 && p[i]!=*it){
+                I.push_back(p[i]);
+                j++;
+            }else if(I[j] != p[i] && p[i] != *it){ 
+                I.push_back(p[i]);
+                j++;
+            }    
+        }
 
 
 }
